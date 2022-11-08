fe6bdc2150036d70d76676b9794637f0397a8e22
1048493 <mayra.carreno@bancoazteca.com>
2022-11-08 14:58:53 -0600

    Add PokedexAPI

--- PokedexAPI/PokedexAPI/PokedexAPI.swift
+++ PokedexAPI/PokedexAPI/PokedexAPI.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+public struct Pokemon: Decodable {
+    
+    public let id: Int
+    public let name: String
+    public let image: String
+    public let weight: Int
+    public let base_experience: Int
+    public let types: [String]
+    public let abilities: [String]
+    
+    public init(id: Int,
+                name: String,
+                image: String,
+                weight: Int,
+                base_experience: Int,
+                types: [String],
+                abilities: [String]) {
+        self.id = id
+        self.name = name
+        self.image = image
+        self.weight = weight
+        self.base_experience = base_experience
+        self.types = types
+        self.abilities = abilities
+    }
+    
+}
+
+public final class PokedexAPI {
+    func getPokemon() -> [Pokemon] {
+        guard let url = Bundle(identifier: "com.Mayra.PokedexAPI")?.url(forResource: "pokemons", withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let pokemons = try? JSONDecoder().decode([Pokemon].self, from: data)
+        else {
+            return []
+        }
+        return pokemons
+    }
+}
