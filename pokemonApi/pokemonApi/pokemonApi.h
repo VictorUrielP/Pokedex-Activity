2759b1f5d4bc04bbbda3c32f366e4ac7361c7b42
victor manzanero loeza <victor.manzanero@globant.com>
2022-11-08 14:35:59 -0600

    my first commit

---
+++ pokemonApi/pokemonApi/pokemonApi.h
@@ -0,0 +1,18 @@
+//
+//  pokemonApi.h
+//  pokemonApi
+//
+//  Created by victor manzanero on 08/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pokemonApi.
+FOUNDATION_EXPORT double pokemonApiVersionNumber;
+
+//! Project version string for pokemonApi.
+FOUNDATION_EXPORT const unsigned char pokemonApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pokemonApi/PublicHeader.h>
+
+
