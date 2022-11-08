90b563ee263a29da99d2597e7ad854c998d472c6
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2022-11-08 14:19:31 -0600

    Created PokedexAPI

---
+++ PokedexAPI/PokedexAPI/PokedexAPI.h
@@ -0,0 +1,18 @@
+//
+//  PokedexAPI.h
+//  PokedexAPI
+//
+//  Created by Alan Emiliano Ramirez Ayala on 08/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokedexAPI.
+FOUNDATION_EXPORT double PokedexAPIVersionNumber;
+
+//! Project version string for PokedexAPI.
+FOUNDATION_EXPORT const unsigned char PokedexAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokedexAPI/PublicHeader.h>
+
+
