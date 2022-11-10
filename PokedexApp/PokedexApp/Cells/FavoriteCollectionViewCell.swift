9d63e2f0aa85b0bde4e5e51521b36285116193fd
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2022-11-10 16:35:41 -0600

    Create Favorite Cell

---
+++ PokedexApp/PokedexApp/Cells/FavoriteCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  PokedexApp
+//
+//  Created by Alan Emiliano Ramirez Ayala on 10/11/22.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokemonImage: UIImageView!
+    
+    @IBOutlet weak var pokemonName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setInformation() {
+        
+    }
+    
+}
